9f21192b6ebdd699fcb84a7a6f17f20e91a0c848
Xavier Olivares <xm.olivares@gmail.com>
2019-06-21 12:23:05 -0400

    feat(speechrecognition): researching audio files and import bugs, unable to go build at this time

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 Goal: Learning how to work with golang, google cloud api, and speech recognition
 
-Resource: https://medium.com/@ODSC/quick-start-for-golang-google-cloud-api-and-speech-recognition-f157cda1ce5c
+RESOURCE -
+https://medium.com/@ODSC/quick-start-for-golang-google-cloud-api-and-speech-recognition-f157cda1ce5c
 
+CREATING A SERVICE ACCOUNT -
+Setting up a Google Cloud API account: https://cloud.google.com/docs/authentication/getting-started#auth-cloud-implicit-go 
+
+SECURITY -
+Managing service account keys: https://cloud.google.com/iam/docs/understanding-service-accounts?hl=en_US&_ga=2.101743105.-1015558750.1559092652#managing_service_account_keys
+
+Securing keys with GODOTENV - 
+https://github.com/joho/godotenv
+
+Still unclear:
+Is my secret configuration working?
+How to get an audio file?
+How to import that file into application?
--- test.go
+++ test.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"golang.org/x/net/context"
+	speech "cloud.google.com/go/speech/apiv1"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+	"github.com/joho/godotenv" 
+	"log" 
+	"os"
+)
+
 
 func main() {
 	fmt.Println("Hello ODSC and ME!")
+
+	//ERROR HANDLING FOR GOOGLE_APPLICATION_CREDENTIALS
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	googleKey := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+	//ERROR HANDLING IF API HAS NOT COMPLETED A REQUEST
+	ctx := context.Background()
+
+	client, err := speech.NewClient(ctx)
+	if(err != nil) {
+	fmt.Println(err)
+	}
+
+	//ERROR HANDLING FOR CONNECTING TO AUDIO FILE
+	filDir := "Recording.wav";
+
+	audioData, err := ioutil.ReadFile(fileDir);
+	if(err != nil) {
+	fmt.Println(err)
+	}
+
+	response, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
+	Config: &speechpb.RecognitionConfig{
+	Encoding: speechpb.RecognitionConfig_LINEAR16,
+	SampleRateHertz: 22050,
+	LanguageCode: "en-US",
+	},
+	Audio: &speechpb.RecognitionAudio{
+	AudioSource: &speechpb.RecognitionAudio_Content{Content: audioData},
+	},
+	})
+
+	if (err != nil) {
+		fmt.Println(err)
+	}
+
+	for _, result := range response.Results{
+	for _, alt := range result.Alternatives{
+	fmt.Println(alt.Transcript)
+	
+	}
+	}
 }
