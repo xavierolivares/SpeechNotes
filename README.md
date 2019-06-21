06e116b8b32507f4d4d0d681a428fc3fdfcbfd77
Xavier Olivares <xm.olivares@gmail.com>
2019-06-21 12:59:29 -0400

    fix(speechtest): first test failed, needed to enable api and set up billing ag account with google

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ How to get an audio file?
 How to import that file into application?
 
 Get the Client Library Tutorial: https://github.com/Drizzy3D/go-speech-recognition-lib
+
+TESTING RESULT 1:
+-2019/06/21 12:54:47 Failed to recognize: rpc error: code = PermissionDenied desc = Cloud Speech-to-Text API has not been used in project "soandso" before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/speech.googleapis.com/overview?project="soandso" then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.
+-Enabled API, needed to set up billing account. Free 12months or $300 credit!
--- test.go
+++ test.go
@@ -1,6 +1,6 @@
 package main
 
-
+import _ "github.com/joho/godotenv/autoload"
 import (
 	"fmt"
 	// "net/http"
@@ -8,9 +8,9 @@ import (
 	"golang.org/x/net/context"
 	speech "cloud.google.com/go/speech/apiv1"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
-	"github.com/joho/godotenv" 
+	// "github.com/joho/godotenv" 
 	"log" 
-	"os"
+	// "os"
 )
 
 
@@ -18,11 +18,11 @@ func main() {
 	fmt.Println("Hello ODSC and ME!")
 
 	//ERROR HANDLING FOR GOOGLE_APPLICATION_CREDENTIALS
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	googleKey := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// googleKey := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 	//ERROR HANDLING IF API HAS NOT COMPLETED A REQUEST
 	ctx := context.Background()
