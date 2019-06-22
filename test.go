1f29c8e24f01413f3a8694e54c53b495a69dad76
Xavier Olivares <xm.olivares@gmail.com>
2019-06-21 23:02:24 -0400

    new(branch): testing textToSpeech

--- test.go
+++ test.go
@@ -1,89 +1,54 @@
 package main
 
-import _ "github.com/joho/godotenv/autoload"
-
 import (
 	"context"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
-	"os"
 
-	speech "cloud.google.com/go/speech/apiv1"
-	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
 func main() {
+	// Instantiates a client.
 	ctx := context.Background()
 
-	client, err := speech.NewClient(ctx)
+	client, err := texttospeech.NewClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	stream, err := client.StreamingRecognize(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Send the initial configuration message.
-	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
-			StreamingConfig: &speechpb.StreamingRecognitionConfig{
-				Config: &speechpb.RecognitionConfig{
-					Encoding:        speechpb.RecognitionConfig_LINEAR16,
-					SampleRateHertz: 16000,
-					LanguageCode:    "en-US",
-				},
-			},
+
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: "Get fucked"},
 		},
-	}); err != nil {
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "en-US",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	go func() {
-		// Pipe stdin to the API.
-		buf := make([]byte, 1024)
-		for {
-			n, err := os.Stdin.Read(buf)
-			if n > 0 {
-				if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-					StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
-						AudioContent: buf[:n],
-					},
-				}); err != nil {
-					log.Printf("Could not send audio: %v", err)
-				}
-			}
-			if err == io.EOF {
-				// Nothing else to pipe, close the stream.
-				if err := stream.CloseSend(); err != nil {
-					log.Fatalf("Could not close stream: %v", err)
-				}
-				return
-			}
-			if err != nil {
-				log.Printf("Could not read from stdin: %v", err)
-				continue
-			}
-		}
-	}()
-
-	for {
-		resp, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Cannot stream results: %v", err)
-		}
-		if err := resp.Error; err != nil {
-			// Workaround while the API doesn't give a more informative error.
-			if err.Code == 3 || err.Code == 11 {
-				log.Print("WARNING: Speech recognition request exceeded limit of 60 seconds.")
-			}
-			log.Fatalf("Could not recognize: %v", err)
-		}
-		for _, result := range resp.Results {
-			fmt.Printf("Result: %+v\n", result)
-		}
+	// The resp's AudioContent is binary.
+	filename := "output.mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
 }
+
