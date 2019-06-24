d65805ca5a13bfa402426e4c5c0aabb759aeb80b
Xavier Olivares <xm.olivares@gmail.com>
2019-06-24 00:32:21 -0400

    fixed some files

--- main.go
+++ main.go
@@ -5,6 +5,5 @@ func main() {
 	// as you start your server, you might run into an issue where the line AFTER starting the server is blocked
 	// gostartServer()
 	// next line doesn't execute
-	// liveCaption()
 	test()
 }
